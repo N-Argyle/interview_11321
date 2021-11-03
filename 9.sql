b6a865af8accbf0b41a774e60a8016082d20cd08
Nicholas Argyle <nick@nicholass-mbp-2.lan>
2021-11-03 10:53:28 -0400

    Add sql question

---
+++ 9.sql
@@ -0,0 +1,10 @@
+-- Given the following tables, write a query that selects firstname, lastname, and photourl for each user
+
+-- users
+-- -------
+-- id (int primary key) | firstname (varchar) | lastname (varchar) | email (varchar)
+
+-- user_photos
+-- ------------
+-- id (int primary key) | photourl (varchar) | userid (int) | deleted (boolean)
+
